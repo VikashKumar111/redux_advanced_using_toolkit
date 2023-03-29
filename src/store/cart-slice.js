abce67d20feb4766ae5d8d240a6827fb86205734
Vikash Kumar <vikasthakur9310@gmail.com>
2023-03-29 20:09:08 +0530

    add

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,5 +1,4 @@
 import { createSlice } from "@reduxjs/toolkit";
-import Cart from "../components/Cart/Cart";
 
 const cartSlice = createSlice({
     name: 'cart',
@@ -12,9 +11,10 @@ const cartSlice = createSlice({
         addItemToCardHandler(state, action) { 
             const newItem = action.payload;
             const existingItem = state.items.find(item => item.id === newItem.id);
+            state.totalQuantity++;
             if (!existingItem) {
                 state.items.push({
-                    itemId: newItem.id,
+                    id: newItem.id,
                     price: newItem.price,
                     quantity: 1,
                     totalPrice: newItem.price,
@@ -28,6 +28,7 @@ const cartSlice = createSlice({
         removeItemFromCart(state, action) {
             const id = action.payload;
             const existingItem = state.items.find(item => item.id === id);
+            state.totalQuantity--;
             if (existingItem === 1) {
                 state.items = state.items.filter(item => item.id !== id )
             } else {
